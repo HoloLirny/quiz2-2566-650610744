62ac3fcdc4b07be6bd37eaef2d79c0797c8cdd6f
Holo <nhuneqw@gmail.com>
2023-08-10 15:04:35 +0700

    kk

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Reply } from "@/components/Reply";
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +7,43 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  let l = false;
+  if (likeNum !== 0) {
+    l = true;
+  }
   return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
+    <div className="d-flex gap-2 my-2">
+      <img
+        src={userImagePath}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#E5E7EB" }}
+      >
+        <span className="fw-semibold">{username}</span>
+        <br />
+        <span>{commentText}</span>
+        {l ? (
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span className="text-muted">{likeNum} คน</span>
+          </div>
+        ) : (
+          ""
+        )}
+        {replies.map((re) => (
+          <Reply
+            userImagePath={re.userImagePath}
+            username={re.username}
+            replyText={re.replyText}
+            likeNum={re.likeNum}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,25 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+import React from "react";
+export const PostOwner = ({ username, title, likeNum }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/นศ.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">{username}</span>
+      </div>
+
+      <span>{title}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span className="text-muted">{likeNum} คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
